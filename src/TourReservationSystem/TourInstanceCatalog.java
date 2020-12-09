ddea848129ab3c6c69b7d8ec262e9f5037e0ecf0
lilit_otaryan@edu.aua.am <lilit_otaryan@edu.aua.am>
2020-12-10 00:19:56 +0400

    Initial commit

--- src/TourReservationSystem/TourInstanceCatalog.java
+++ src/TourReservationSystem/TourInstanceCatalog.java
@@ -1,5 +1,20 @@
 package TourReservationSystem;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TourInstanceCatalog {
+
+	private Map <String, TourInstance> tours = new HashMap<String, TourInstance>();
+	
+	public TourInstanceCatalog(){};
+	
+	public void addTourInstance(TourInstance tourInstance) {
+		tours.put(tourInstance.getTourInstanceID(), tourInstance);
+	};
+	
+	public void removeTourInstance(TourInstance tourInstance) {
+		
+	};
 
 }
